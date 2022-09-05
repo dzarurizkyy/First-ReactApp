d1488680fd0bf55ab1e4c940320f460271171efe
dzarurizkyy <dzarurizkybusiness@gmail.com>
2022-09-05 17:58:01 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>HariSenin Batch 3</h1>
+        <h2>HariSenin Batch 3</h2>
+        <h3>HariSenin Batch 3</h3>
+        <h4>HariSenin Batch 3</h4>
+        <h5>HariSenin Batch 3</h5>
+        <h6>HariSenin Batch 3</h6>
+    </div>
+  );
+}
+
+export default App;
